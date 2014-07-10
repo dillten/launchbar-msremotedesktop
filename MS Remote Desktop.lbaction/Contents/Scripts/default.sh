b1de0242e5c7236d7ad4f4bb4d15e046edcbf701
dillten <dillon@tenbrink.us>
2014-07-10 16:39:05 -0500

    1.01
    Updated action to launch windowed (1024x768).

--- MS Remote Desktop.lbaction/Contents/Scripts/default.sh
+++ MS Remote Desktop.lbaction/Contents/Scripts/default.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 EXP=$1
-open "rdp://full%20address=s:$EXP:3389&authentication%20level=i:0"
+open "rdp://full%20address=s:$EXP:3389&authentication%20level=i:0&desktopwidth=i:1024&desktopheight=i:768&screen%20mode%20id=i:1"
 exit 0
